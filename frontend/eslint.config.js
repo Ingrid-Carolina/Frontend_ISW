b12adcc391f98bde59b71712cf8f5a3dedf0ef38
KennethEspinoza14 <Kennethespinoza@unitec.edu>
2025-05-21 15:33:41 -0600

    Agregada configuración de ESLint y Prettier al proyecto

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,33 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+export default defineConfig([
+	{
+		ignores: [
+			'node_modules',
+			'dist',
+      'dist-ssr',
+			'build',
+			'coverage',
+			'public',
+			'**/*.test.*',
+			'**/*.spec.*',
+			'vite.config.js*',
+			'*.config.js',
+			'.*.js'
+		],
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				ecmaFeatures: { jsx: true },
+			},
+			globals: globals.browser,
+		},
+		plugins: {
+			react: pluginReact,
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			...pluginReact.configs.flat.recommended.rules,
+			'react/react-in-jsx-scope': 'off',
+			'react/jsx-no-target-blank': 'warn',
+		},
+	},
+	prettier,
+]);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <h1>Hola mundo</h1>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<h1>Hola mundo</h1>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
