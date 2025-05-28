c259be21b1dc260e1de7d6bb7ae8ae94a19aca46
Junnior <junniorgsauceda@gmail.com>
2025-05-27 22:24:11 -0600

    Subpagina Logros y Voluntariado creadas

---
+++ frontend/src/pages/Logros.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logros = () => {
+
+  return (
+    <div style={{ padding: '2rem', color: 'white' }}>
+      <h1>Nuestros Logros</h1>
+      <p>Bienvedido, a continuacion conocera sobre todos los logros que nuestra organización ha alcanzado a lo largo de los años</p>
+    
+    </div>
+  );
+};
+
+export default Logros;
---
+++ frontend/src/pages/Voluntariado.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Voluntariado = () => {
+
+  return (
+    <div style={{ padding: '2rem', color: 'white' }}>
+      <h1>Voluntariado</h1>
+      <p>Bienvenido a nuestra sección de voluntariado, cualquier apoyo que nos proporcione será grandemente agradecido.</p>
+    </div>
+  );
+};
+
+export default Voluntariado;
