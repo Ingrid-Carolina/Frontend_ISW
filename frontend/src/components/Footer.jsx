9e5d00996daf9fb5fa2c09632fa777dd5c249a2c
Carlos Flores <carlosfernandoflores321@gmail.com>
2025-05-29 10:26:23 -0600

    corrigiendo error del footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@
 
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import logo from '../ima/Logo-pilotos.png';
+import logo from '../Ima/Logo-pilotos.png';
 import Box from '@mui/material/Box';
 import '../components/styles.css';
 
