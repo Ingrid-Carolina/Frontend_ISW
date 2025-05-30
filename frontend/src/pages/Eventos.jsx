e42b1f4bac0b9ee436e06a5b3266e21c3aa0dfd5
Carlos Flores <carlosfernandoflores321@gmail.com>
2025-05-30 10:55:31 -0600

    start eventos

--- frontend/src/pages/Eventos.jsx
+++ frontend/src/pages/Eventos.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
+import { Box, Typography, TextField, Button, Grid, Paper, Divider } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search'
+
+
+//array de events, aqui se haran store desde el backend en el futuro
+
+const eventos = [
+	{
+		fecha: '2025-05-30',
+		titulo: '2025 Central Region Tournament Preparation Umpire Clinic',
+		lugar: '7185 S. Indianapolis Road, Whitestown, IN',
+		hora: 'May 30 @ 1:00 pm - June 1 @ 12:00 pm',
+	},
+	{
+		fecha: '2025-06-15',
+		titulo: 'Entrenamiento Regional de Verano',
+		lugar: 'Tegucigalpa, Honduras',
+		hora: 'June 15 @ 10:00 am - 4:00 pm',
+	},
+]
 
 const Eventos = () => {
+
+	//para filtrar
+	const [query, setQuery] = usestate('');
+
+	// filtra por lo que se busq
+	const filteredEvents = eventos.filter(e => e.titulo.toLowerCase().includes(query.toLowerCase()));
+
 	return (
 		<div style={{ padding: '2rem', color: 'black' }}>
 			<h1>Evento</h1>
