da570fcb7be956cbf6a9453ace309fced443f638
Ingrid <incahernandez0802@gmail.com>
2025-06-19 08:27:27 -0600

    modificacion

--- frontend/src/pages/Tienda.jsx
+++ frontend/src/pages/Tienda.jsx
@@ -733,32 +733,28 @@ buscarPorCategoria = (producto, searchTerm) => {
         py: 4,
         maxHeight: 'calc(100vh + 250px)',
         overflow: 'auto',
-        /*scrollbarWidth: 'none',
-        '&::-webkit-scrollbar': {
-         display: 'none'
-      },
-        '-ms-overflow-style': 'none',*/
-        // SCROLL PERSONALIZADO TEMA PILOTOS
-         '&::-webkit-scrollbar': {
-    width: '12px',
-  },
-  '&::-webkit-scrollbar-track': {
-    background: '#f8f9fa',
-    borderRadius: '20px',
-    boxShadow: 'inset 0 0 5px rgba(0,0,0,0.1)',
-  },
-  '&::-webkit-scrollbar-thumb': {
-    background: 'linear-gradient(45deg, #667eea 0%, #764ba2 100%)',
-    borderRadius: '20px',
-    boxShadow: '0 2px 10px rgba(0,0,0,0.3)',
-    border: '2px solid #f8f9fa',
-    '&:hover': {
-      background: 'linear-gradient(45deg, #764ba2 0%, #667eea 100%)',
-      boxShadow: '0 4px 15px rgba(0,0,0,0.4)',
-    },
-  },
-  scrollbarWidth: 'auto',
-  scrollbarColor: '#667eea #f8f9fa',
+        
+        // SCROLL Prdocuto de  PILOTOS
+            '&::-webkit-scrollbar': {
+            width: '12px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: '#f8f9fa',
+            borderRadius: '20px',
+            boxShadow: 'inset 0 0 5px rgba(0,0,0,0.1)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'linear-gradient(45deg, #667eea 0%, #764ba2 100%)',
+            borderRadius: '20px',
+            boxShadow: '0 2px 10px rgba(0,0,0,0.3)',
+            border: '2px solid #f8f9fa',
+            '&:hover': {
+              background: 'linear-gradient(45deg, #764ba2 0%, #667eea 100%)',
+              boxShadow: '0 4px 15px rgba(0,0,0,0.4)',
+            },
+          },
+          scrollbarWidth: 'auto',
+          scrollbarColor: '#667eea #f8f9fa',
         paddingBottom: '80px'
       }}
     >
@@ -1169,3 +1165,10 @@ buscarPorCategoria = (producto, searchTerm) => {
 }
 
 export default Tienda;
+
+//el scroll anterior de Producto
+/*scrollbarWidth: 'none',
+        '&::-webkit-scrollbar': {
+         display: 'none'
+      },
+        '-ms-overflow-style': 'none',*/
