958b211c7c8d2d51a691560cbb07376fbece4953
KennethEspinoza14 <Kennethespinoza@unitec.edu>
2025-09-02 16:08:32 -0600

    prueba de formulario sin captcha fix temporal

--- frontend/src/pages/Contacto.jsx
+++ frontend/src/pages/Contacto.jsx
@@ -47,23 +47,23 @@ const Contacto = () => {
 	const onSubmit = async (data, e) => {
 		e.preventDefault();
 
-		const token = await captcha.current.executeAsync();
-		captcha.current.reset();
-		if (!token) {
-			window.alert('Por favor, valida el CAPTCHA.');
-			return;
-		}
+		// const token = await captcha.current.executeAsync();
+		// captcha.current.reset();
+		// if (!token) {
+		// 	window.alert('Por favor, valida el CAPTCHA.');
+		// 	return;
+		// }
 
 		// Verificar token con el backend
 		try {
-			const res = await api.post('/auth/verificar', { token });
+			// const res = await api.post('/auth/verificar', { token });
 
-			if (!res.data.success) {
-				setSnackbarType('error');
-				setSnackbarMsg('Verificación del CAPTCHA fallida.');
-				setOpenSnackbar(true);
-				return;
-			}
+			// if (!res.data.success) {
+			// 	setSnackbarType('error');
+			// 	setSnackbarMsg('Verificación del CAPTCHA fallida.');
+			// 	setOpenSnackbar(true);
+			// 	return;
+			// }
 
 			// Procede con el envío del formulario
 			const body = {
@@ -478,11 +478,11 @@ const Contacto = () => {
 						</Box>
 
 						{/* BOTÓN ENVIAR MENSAJE*/}
-						<ReCAPTCHA
+						{/* <ReCAPTCHA
 							ref={captcha}
 							sitekey='6LeoJWErAAAAAL6RcLtqe59DOJyUGdkQO1gc3Nvm'
 							size='invisible'
-						/>
+						/> */}
 
 						<Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-start' }}>
 							<Button
