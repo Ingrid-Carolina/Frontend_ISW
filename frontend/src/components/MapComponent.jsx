dac60ecbc131953fe0b2e09417bc9926ad2e0cb9
Junnior <junniorgsauceda@gmail.com>
2025-09-03 08:11:10 -0600

    marcador Ubicacion

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -3,26 +3,24 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapComponent.css';
 import PropTypes from 'prop-types';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+ import markerIcon from 'leaflet/dist/images/marker-icon.png';
+ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const MapComponent = ({ isInteractive = true }) => {
-	const mapRef = useRef(null);
-	// const latRef = useRef(null);
-	// const lonRef = useRef(null);
+  const mapRef = useRef(null);
 
-	useEffect(() => {
-		const coord = [14.051398595688246, -87.21678472908647];
-		mapRef.current = L.map('map').setView(coord, 15);
+  useEffect(() => {
+    const coord = [14.051398595688246, -87.21678472908647];
+    mapRef.current = L.map('map').setView(coord, 15);
 
-		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 19,
-			attribution:
-				'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-		}).addTo(mapRef.current);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(mapRef.current);
 
-		// Configurar ícono por defecto
+   // Configurar ícono por defecto
    const DefaultIcon = L.icon({
      iconUrl: markerIcon,
      iconRetinaUrl: markerIcon2x,
@@ -30,57 +28,15 @@ const MapComponent = ({ isInteractive = true }) => {
    });
    L.Marker.prototype.options.icon = DefaultIcon;
 
-		const marker = L.marker(coord).addTo(mapRef.current);
-		marker
-			.bindPopup(
-				'<b>Asociación de Béisbol Menor - FAH</b><br>Tegucigalpa, Honduras',
-			)
-			.openPopup();
+    const marker = L.marker(coord).addTo(mapRef.current);
+    marker
+      .bindPopup(
+        '<b>Asociación de Béisbol Menor - FAH</b><br>Tegucigalpa, Honduras',
+      )
+      .openPopup();
 
-		return () => {
-			mapRef.current.remove();
-		};
-	}, []);
-
-	useEffect(() => {
-		if (!mapRef.current) return;
-
-		if (isInteractive) {
-			mapRef.current.dragging.enable();
-			mapRef.current.scrollWheelZoom.enable();
-			mapRef.current.doubleClickZoom.enable();
-			mapRef.current.boxZoom.enable();
-			mapRef.current.keyboard.enable();
-			if (mapRef.current.tap) mapRef.current.tap.enable();
-			mapRef.current.touchZoom.enable();
-		} else {
-			mapRef.current.dragging.disable();
-			mapRef.current.scrollWheelZoom.disable();
-			mapRef.current.doubleClickZoom.disable();
-			mapRef.current.boxZoom.disable();
-			mapRef.current.keyboard.disable();
-			if (mapRef.current.tap) mapRef.current.tap.disable();
-			mapRef.current.touchZoom.disable();
-		}
-	}, [isInteractive]);
-
-	/**
-	 * Handle wheel event
-	 * @param {React.WheelEvent} event - The wheel event object
-	 */
-	const wheelMoving = event => {
-		event.preventDefault();
-	};
-
-	return (
-		<div onWheel={wheelMoving} className='map-container'>
-			<div id='map'></div>
-		</div>
-	);
-};
-
-MapComponent.propTypes = {
-	isInteractive: PropTypes.bool,
-};
-
-export default MapComponent;
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+}
